588757a37bb9ac9737cfc0cd93f76d8505289e58
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2022-09-21 15:36:55 +0100

    deleted deploy.yml file and modified Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://zayyadms.github.io'
+    site: 'https://z-ms.github.io'
 });
