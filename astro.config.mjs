3d80062ac7b21385a79263771eceb0f21b49c927
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2022-09-21 15:29:40 +0100

    edited astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://z-ms.github.io',
-    base: '/zayyadms'
+    site: 'https://z-ms.github.io'
 });
