d79c1f91bdb02a4abd300e47513977d6c5b61f0c
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2023-12-21 14:19:52 +0100

    Updated to Astro 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://z-ms.github.io'
+    site: 'https://z-ms.github.io',
+    markdown: {
+        shikiConfig: {
+            theme: 'dracula',
+            langs: ['javascript'],
+            wrap: true
+        }
+    }
 });
